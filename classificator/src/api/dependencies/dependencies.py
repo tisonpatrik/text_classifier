33ee14a7e371a1ee9e6934904136ec77b8b3a907
tisonpatrik <patriktison@gmail.com>
2024-05-22 12:30:27 +0000

    chore: Add my_model directory to Dockerfile and dependencies.py

--- classificator/src/api/dependencies/dependencies.py
+++ classificator/src/api/dependencies/dependencies.py
@@ -10,14 +10,13 @@ from common.src.llm_model.custom_model import CustomModel
 from common.src.logging.logger import AppLogger
 
 logger = AppLogger.get_instance().get_logger()
+load_dotenv()
 
 
 def get_open_ai_model():
 	try:
-		load_dotenv()
 		api_key_env = os.getenv('OPENAI_API_KEY')
 		if not api_key_env:
-			logger.error('OPENAI_API_KEY environment variable is not set.')
 			raise ValueError('OPENAI_API_KEY environment variable is not set.')
 
 		api_key = SecretStr(api_key_env)
@@ -34,6 +33,9 @@ def get_open_ai_model():
 def get_ollama():
 	try:
 		ollama_model = os.getenv('OLLAMA_MODEL')
+		logger.debug(f'OLLAMA_MODEL value: {ollama_model}')
+		if ollama_model is None:
+			raise ValueError("OLLAMA_MODEL environment variable not set")
 		model = Ollama(model=ollama_model, base_url='http://ollama:11434', temperature=0)
 		logger.info('Successfully created Ollama model instance.')
 		return model
@@ -42,22 +44,24 @@ def get_ollama():
 		raise
 
 def get_custom_model():
-	try:
-		custom_model = os.getenv('CUSTOM_MODEL')
-		model = CustomModel(model_name=custom_model)
-		logger.info('Successfully created CustomModel instance.')
-		return model
-	except Exception as e:
-		logger.error(f'Error occurred while creating CustomModel: {e}')
-		raise
+    try:
+        custom_model = os.getenv('CUSTOM_MODEL')
+        logger.debug(f'CUSTOM_MODEL value: {custom_model}')
+        if custom_model is None:
+            raise ValueError("CUSTOM_MODEL environment variable not set")
+        model = CustomModel(model_name=custom_model)
+        logger.info('Successfully created CustomModel instance.')
+        return model
+    except Exception as e:
+        logger.error(f'Error occurred while creating CustomModel: {e}')
+        raise
+
 
 
 def get_labels_service():
 	try:
 		labels_file = os.getenv('LABELS_FILE')
-		print(labels_file)
 		labels = load_csv(labels_file)
-		print(labels)
 		logger.info('Successfully loaded labels.')
 		return LabelsService(labels)
 	except Exception as e:
